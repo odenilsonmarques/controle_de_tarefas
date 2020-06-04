c8628fb01124da541bac3c2b3fa2c58e3fade1a9
odenilson marques <odmarquesgm@gmail.com>
2020-06-04 11:00:04 -0300

    alteracao na pagina home do projeto

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,12 +5,14 @@
         <meta name="description" content="@yield('description')">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{('assets/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{('assets/css/estiloHeader.css')}}">
+        
     </head>
     <body>
         <header>
             <!--informando que nessa parte irá ficar o cabeçalho-->
-            <nav class="navbar navbar-default">
-                <div class="container-fluid" style="background-color:#FF8C00">
+            <nav class="navbar navbar-default navbar-fixed-top">
+                <div class="container-fluid">
                     <div class="container">
                         <div class="navbar-header">
                             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavBar">
@@ -23,15 +25,19 @@
                         <!--informando o  ficarrá dentro do cabeçalho-->
                         <div class="navbar-collapse collapse" id="myNavBar">
                             <ul class="nav navbar-nav navbar-right">
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Projetos</a></li>
-                                <li><a href="#">Contatos</a></li>
-                                <li><a href="#">Sobre</a></li>
+                                <li><a href="#">Home <span class="glyphicon glyphicon-home logo-small" style="color:#FFF;font-size:15px"></span></a></li>
+                                <li><a href="#">Projetos <span class="glyphicon glyphicon-th logo-small" style="color:#FFF;font-size:15px"></span></a></li>
+                                <li><a href="#">Contatos <span class="glyphicon glyphicon-earphone logo-small" style="color:#FFF;font-size:15px"></span></a></li>
+                                <li><a href="#">Sobre <span class="glyphicon glyphicon-tasks logo-small" style="color:#FFF;font-size:15px"></span></a></li>
                             </ul>
                         </div>   
                     </div>
                 </div>
-            </nav>
+            </nav><br>
+            <div class="jumbotron text-center">
+                    <h2>Control Projetos</h2><br>
+                    <h3>Controle seus projetos na palma da mão</h3>
+            </div>
         </header>
         <script type="text/javascript" src="{{('assets/js/jquery-3.1.1.min.js')}}">
         <script type="text/javascript" src="{{('assets/js/bootstrap.min.js')}}">
